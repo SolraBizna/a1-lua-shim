8ecf94d6c62b3d848e7cb54ac3641f9c6193536f
Solra Bizna <solra@bizna.name>
2023-07-25 16:38:28 -0600

    Untested initial commit

---
+++ -a1-lua-shim.lua
@@ -0,0 +1,99 @@
+-- Copyright 2023 Solra Bizna. I expressly authorize you (the reader) to use
+-- this script, change it to fit your needs, strip out my name and claim it as
+-- your own, whatever. This copyright claim is solely to assert authorship long
+-- enough to immediately disclaim all copy-rights.
+
+-- Part 0: Don't let me call pairs() [by accident]. I hate pairs().
+
+_G.danger_pairs = pairs
+pairs = nil
+
+-- Part 1: Create a system that allows triggers to be created cooperatively.
+-- AND some safety stuff that will make it so that we won't do things the old
+-- way by accident and ruin everything.
+
+local shadow_triggers = {}
+local the_real_triggers = {}
+local subtriggers = {}
+
+Triggers = nil
+
+setmetatable(_G, {
+   __index = {Triggers = shadow_triggers},
+   __newindex = function(t, key, value)
+      if k == "Triggers" then
+         for key, value in danger_pairs(value) do
+            shadow_triggers[key] = value
+         end
+      else
+         rawset(_G, key, value)
+      end
+   end,
+})
+
+setmetatable(shadow_triggers, {
+   __index = the_real_triggers,
+   __newindex = function(t, key, value)
+      if the_real_triggers[key] == nil then
+         the_real_triggers[key] = function(...)
+            for _, subtrigger in ipairs(subtriggers[key]) do
+               local success, result = pcall(subtrigger, ...)
+               if not success then
+                  -- There was a message. Print the error message.
+                  print(result)
+               elseif result == false then
+                  -- If the subtrigger explicitly returned false, don't call
+                  -- any more subtriggers.
+                  break
+               end
+            end
+         end
+      end
+      if subtriggers[key] == nil then
+         subtriggers[key] = {}
+      end
+      table.insert(subtriggers[key], value)
+   end,
+   __call = function(me, t)
+      for key, value in danger_pairs(t) do
+         me[key] = value
+      end
+   end,
+})
+
+-- Part 2: Make print print *both* to the command line *and* to the screen,
+-- even in Triggers.init(), even in the top level.
+
+local messages_to_print_on_first_idle = {}
+local real_print = print
+function print(...)
+   -- print prints its arguments separated by tabs
+   local text = table.concat({...}, "\t")
+   real_print(text)
+   if messages_to_print_on_first_idle ~= nil then
+      table.insert(messages_to_print_on_first_idle, text)
+   else
+      Players.print(text)
+   end
+end
+
+function Triggers.idle()
+   if messages_to_print_on_first_idle ~= nil then
+      for _, message in ipairs(messages_to_print_on_first_idle) do
+         Players.print(message)
+      end
+      messages_to_print_on_first_idle = nil
+   end
+end
+
+-- Part 3: There's no downside to calling the restore_* functions if they're
+-- not needed. They need to be called at least once per init() if they are
+-- needed. So let's call them ourselves!
+
+function Triggers.init(restoring_game)
+   if restoring_game then
+      Game.restore_saved()
+   else
+      Game.restore_passed()
+   end
+end
---
+++ README.md
@@ -0,0 +1,65 @@
+This is a simple Lua script for the [Aleph One][1] engine. It makes it easier for multiple scripts to cooperate in the same context. It is intended to be especially helpful for scenario scripting.
+
+[1]: https://github.com/Aleph-One-Marathon/alephone/
+
+# Usage
+
+Add this as the first script in your level/plugin/whatever. Make sure it runs before any other scripts.
+
+In each script, you can declare triggers either of the ways that normally work in Aleph One. Piecemeal:
+
+```lua
+Triggers = {}
+
+function Triggers.player_damaged(victim)
+    victim.life = 300
+end
+
+function Triggers.idle()
+   Players.print("Console spam!!!")
+end
+```
+
+Or using the all-at-once method:
+
+```lua
+Triggers = {
+   player_damaged = function(victim)
+      victim.life = 300
+   end,
+   idle = function()
+      Players.print("Console spam!!!")
+   end,
+}
+```
+
+This shim also provides an optional, extra classy method:
+
+```lua
+Triggers {
+   player_damaged = function(victim)
+      victim.life = 300
+   end,
+   idle = function()
+      Players.print("Console spam!!!")
+   end,
+}
+```
+
+Triggers will be called, with all the normal parameters, in whatever order they are provided. If scripts A, B, and C all provide an `idle` trigger, then A's `idle` will be called first, B's second, and C's third. If a trigger has an error, the next triggers will still be called; in the previous example, if B's `idle` trigger throws a Lua error, C's `idle` trigger will still be called.
+
+You can prevent subsequent triggers from being called by explicitly returning `false` from a trigger (e.g. if you prevented damage and don't want the next `player_damaged` trigger to be called).
+
+# Bonus Features
+
+## Danger Pairs
+
+`pairs` gets its name changed to `danger_pairs`, so that casual use will be caught. `pairs` is a very useful function, irreplaceable for some purposes, and when used properly it can grant nice performance with no problems. However, careless use will cause films to desync. Thus, this shim renames it to `danger_pairs`. If you need it, and you're sure you're not going to cause a desync by using it, just use the longer name and you're all set.
+
+## Print
+
+This script makes it so that `print` will print both to the screen *and* to the Aleph One log. It will even work during `Triggers.init` or at the top level of the script.
+
+# Legalese
+
+Copyright 2023 Solra Bizna. I expressly authorize you (the reader) to use this script, change it to fit your needs, strip out my name and claim it as your own, whatever. This copyright claim is solely to assert authorship long enough to immediately disclaim all copy-rights.
